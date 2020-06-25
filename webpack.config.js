5b292fb60c813f0fd923343ecbd5fe2540c38cf3
Kacper Walanus <kacper@walanus.com>
2020-06-25 16:19:23 +0200

    add watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  devtool: 'inline-source-map',
   entry: './src/index.js',
+  mode: process.env.NODE_ENV,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
