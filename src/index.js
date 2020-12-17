5a1be0f1796d78586fac20fac93811677d2a1c02
thiefunny <mikolaj@walanus.com>
2020-12-17 15:10:24 +0100

    Mikołaj's config of webpack for local live tests

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ require("jquery")
 require("./app")
 require("./search")
 require("./clear-button")
+import facebook_icon from './img/facebook_icon.png'
+import google_icon from './img/google_icon.png'
+import yelp_icon from './img/yelp_icon.png'
+import zomato_icon from './img/zomato_icon.png'
+// import './scss/main.scss'
+import './main.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -8,4 +13,39 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+
+  plugins: [new HtmlWebpackPlugin({
+      title: 'webpack',
+      template: path.resolve(__dirname, './src/index.html'), // template file      
+      filename: 'index.html', // output file    
+    }),
+    new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin(),
+  ],
+
+  module: {
+    rules: [
+      // images
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img'
+          },
+        }, ],
+      },
+      //css
+      {
+        test: /\.css$/,
+        use: [
+          // MiniCssExtractPlugin.loader, 
+          'style-loader',
+          'css-loader'],
+      },
+    ]
+  }
+
 };
