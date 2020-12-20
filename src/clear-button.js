138fe9b8cd5fb3e63fa4be5fefe83d609fa2d5e8
thiefunny <mikolaj@walanus.com>
2020-12-20 06:59:48 +0100

    removed jQuery from all code

--- src/clear-button.js
+++ src/clear-button.js
@@ -1,14 +1,4 @@
-// $(function () {
-//     $("#clear-button").on("click", () => {
-//         $("#pac-input").val(null);
-//         $("#pac-input").focus();
-//     })
-// });
-
 document.querySelector("#clear-button").addEventListener("click", _ => {
-    // $("#pac-input").val(null);
     document.querySelector("#pac-input").value = null;
-    // $("#pac-input").focus();
+    document.querySelector("#pac-input").focus;
 })
-
-// console.log(document.querySelector("#clear-button"));
