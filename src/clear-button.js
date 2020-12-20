135a5af8bfd28e69faa6aca03c044544a8c8994d
thiefunny <mikolaj@walanus.com>
2020-12-20 07:06:46 +0100

    zmieniony header

--- src/clear-button.js
+++ src/clear-button.js
@@ -1,4 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
 document.querySelector("#clear-button").addEventListener("click", _ => {
     document.querySelector("#pac-input").value = null;
     document.querySelector("#pac-input").focus;
-})
+})})
